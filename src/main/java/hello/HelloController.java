385674e8be9db59c827d101ece66f251b5600b6a
netjagan <netjagan@gmail.com>
2019-09-27 17:01:13 +0800

    try

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/test")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! truie!";
     }
     
 }
